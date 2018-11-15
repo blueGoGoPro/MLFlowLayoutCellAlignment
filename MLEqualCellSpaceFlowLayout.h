5e2184c88f59f230cf44ff5c6bd6f9313b2efe49
yanglei <yanglei@268xue.com>
2018-11-15 09:56:45 +0800

    修改collectionview的布局

---
+++ MLEqualCellSpaceFlowLayout.h
@@ -0,0 +1,25 @@
+//
+//  MLEqualCellSpaceFlowLayout.h
+//  UICollectionViewDemo
+//
+//  Created by leilei on 2017/10/13.
+//  Copyright © 2017年 CHC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+typedef NS_ENUM(NSInteger,AlignType){
+    AlignWithLeft,
+    AlignWithCenter,
+    AlignWithRight
+};
+@interface MLEqualCellSpaceFlowLayout : UICollectionViewFlowLayout
+//两个Cell之间的距离
+@property (nonatomic,assign)CGFloat betweenOfCell;
+//cell对齐方式
+@property (nonatomic,assign)AlignType cellType;
+
+-(instancetype)initWthType : (AlignType)cellType;
+//全能初始化方法 其他方式初始化最终都会走到这里
+-(instancetype)initWithType:(AlignType) cellType betweenOfCell:(CGFloat)betweenOfCell;
+
+@end
